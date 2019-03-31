ac82104632081fa5daa1bfb34b56a55f25f494db
jimit09.gandhi <jimit.silenthill@gmail.com>
2019-03-31 18:29:15 +0530

    Added algorithm to find the cycle in undirected graph using Disjoint set.

---
+++ Algorithms.UnitTests/GraphTests/UnionFinderTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Algorithms.Data_Structures.Graph.UnionFinder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.UnitTests.GraphTests
+{
+    [TestClass]
+    public class UnionFinderTest
+    {
+        [TestMethod]
+        public void Should_GraphWithCycles_CheckHasCycles_ReturnsHasCycles()
+        {
+            //Arrange
+            Graph graph = new Graph(3, 3);
+
+            graph.Edges[0].Source = 0;
+            graph.Edges[0].Destination = 1;
+
+            graph.Edges[1].Source = 1;
+            graph.Edges[1].Destination = 2;
+
+            graph.Edges[2].Source = 0;
+            graph.Edges[2].Destination = 2;
+
+            //Act  
+            var hasCycle = graph.HasCycle();
+
+            //Assert    
+            Assert.IsTrue(hasCycle);
+        }
+    }
+}
---
+++ Algorithms/Data Structures/Graph/UnionFinder/Graph.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Data_Structures.Graph.UnionFinder
+{
+    public class Graph
+    {
+        public int VerticesCount { get; set; }
+        public int EdgesCount { get; set; }
+
+        public Edge[] Edges { get; set; }
+
+        public class Edge
+        {
+            public int Source { get; set; }
+            public int Destination { get; set; }
+        }
+
+        public Graph(int v, int e)
+        {
+            this.VerticesCount = v;
+            this.EdgesCount = e;
+            this.Edges = new Edge[e];
+            for (int i = 0; i < this.EdgesCount; i++)
+            {
+                this.Edges[i] = new Edge();
+            }
+        }
+
+        public int Find(int[] parent, int i)
+        {
+            if (parent[i] == -1)
+            {
+                return i;
+            }
+            return Find(parent, parent[i]);
+        }
+
+        public bool HasCycle()
+        {
+            int[] parent = new int[VerticesCount];
+            for (int i = 0; i < EdgesCount; i++)
+            {
+                parent[i] = -1;
+            }
+
+            for (int i = 0; i < EdgesCount; i++)
+            {
+                int x = Find(parent, Edges[i].Source);
+                int y = Find(parent, Edges[i].Destination);
+
+                if (x == y)
+                {
+                    return true;
+                }
+
+                Union(parent, x, y);
+            }
+            return false;
+        }
+
+        private void Union(int[] parent, int x, int y)
+        {
+            int a = Find(parent, x);
+            int b = Find(parent, y);
+            parent[a] = b;
+        }
+    }
+}
